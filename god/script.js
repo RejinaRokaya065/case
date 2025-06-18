95edf145c15c432c5cb785279e83756b170ad618
RejinaRokaya065 <rokayarejina1@gmail.com>
2025-06-18 10:03:25 +0545

    update

--- god/script.js
+++ god/script.js
@@ -2,7 +2,12 @@
     const models = {
         samsung: ["Galaxy S21", "Galaxy S22", "Galaxy A52"],
         apple: ["iphone 11","iphone 11 PRO","iphone 11 pro","iPhone 13", "iPhone 14", "iPhone SE"],
-        xiaomi: ["Redmi Note 10", "Redmi Note 11", "Mi 11"]
+        xiaomi: ["Redmi Note 10", "Redmi Note 11", "Mi 11"],
+        vivo:["vivo v11","vivo v15 pro","vivo v17","vivo v19","vivo v17 pro"],
+        huwaei:["huawei nova 4","huawei Y5 2019","huwaei Y6 2019","huwaei nova 21"],
+        realme:["Realme 5/5l/5S","Realme7","Realme 71","Realme 71"],
+        oppo:["oppo A1K","oppo A31","oppo A53","oppo A52/92","oppo A39"],
+        oneplus:["oneplus 10 pro","oneplus 11R","oneplus 5","oneplus 12","oneplus 6","oneplus 9","oneplus 9 pro"]
     };
 
     document.getElementById("brand").addEventListener("change", function() {
